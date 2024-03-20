3c1175372641daf82da078424de0015105e35019
goshkiev <goshkievmikhail@gmail.com>
2024-03-20 15:44:02 +0300

    hack m added

--- components/MyVideo.tsx
+++ components/MyVideo.tsx
@@ -1,8 +1,15 @@
 'use client'
 import Video from "next-video";
 import videoLoop from "../videos/part_01 scn_05 web_v2.mp4";
+import { useEffect, useState } from "react";
 
 export const MyVideo = () => {
+  const [count, setCount] = useState(0);
+  // hack
+  useEffect(() => {
+    setCount(1)
+  }, [count, setCount]);
+
   return (
     <Video
       style={{
@@ -12,7 +19,7 @@ export const MyVideo = () => {
         width: "100vw",
       }}
       src={videoLoop}
-      autoPlay={true}
+      autoPlay={count === 1}
       loop
       playsInline
       preload="auto"
